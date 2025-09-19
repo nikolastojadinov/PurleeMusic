4f9fd989665fdcc1172d503dce6f110e6afc0d12
nikolastojadinov <nikolastojadinov@yahoo.co.uk>
2025-09-19 08:06:35 +0000

    fix: remove unused useRef import (lint error)

--- client/src/components/MusicPlayer.tsx
+++ client/src/components/MusicPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import './MusicPlayer.css';
 import { usePlayer } from './PlayerContext';
 
